fc84858f9f0e7cd1128d913a2734aacb7b2fefba
honglygit <36412970+honglygit@users.noreply.github.com>
2019-03-16 13:35:50 +0700

    initial commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Homepage</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <style>
+    .fakeimg {
+        height: 200px;
+        background: #aaa;
+    }
+    </style>
+</head>
+<body>
+
+    <div class="jumbotron text-center" style="margin-bottom:0">
+        <h1>Booklet</h1>
+    </div>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>    
+        </ul>
+    </div>  
+    </nav>
+
+    <div class="container" style="margin-top:30px">
+        <div class="row">
+            <div class="col-xl col-lg-3 col-md-4 col-sm-6 pb-4">
+                <div class="card card-block">
+                    <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG" alt="Card image cap">
+                    <div class="card-body" style="margin:30px">
+                        <h5>Tittle</h5>
+                        <p class="card-text">Some quick example text to build on the
+                             card title and make up the bulk of the card's content.</p>
+                        <p style="color:#808e95">yesterday, 23:22</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl col-lg-3 col-md-4 col-sm-6 pb-4">
+                <div class="card card-block">
+                    <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG" alt="Card image cap">
+                    <div class="card-body" style="margin:30px">
+                        <h5>Tittle</h5>
+                        <p class="card-text">Some quick example text to build on the
+                             card title and make up the bulk of the card's content.</p>
+                        <p style="color:#808e95">yesterday, 23:22</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl col-lg-3 col-md-4 col-sm-6 pb-4">
+                <div class="card card-block">
+                    <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG" alt="Card image cap">
+                    <div class="card-body" style="margin:30px">
+                        <h5>Tittle</h5>
+                        <p class="card-text">Some quick example text to build on the
+                             card title and make up the bulk of the card's content.</p>
+                        <p style="color:#808e95">yesterday, 23:22</p>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>
+    <div class="row">
+        <div class="offset-md-9">
+            <a href="#" class="badge badge-light" style="font-size:24px;color:#808e95">More features </a>
+        </div>
+    </div>
+
+
+    {{-- i will do tmrrrrrrrrrrrrrrrrrrrrr --}}
+    
+
+    <div class="jumbotron text-center" style="margin-bottom:0">
+        <p>Footer</p>
+    </div>
+
+</body>
+</html>

5f8e329bf7f3ed8cb7fdbc661988a72618679eec
honglygit <36412970+honglygit@users.noreply.github.com>
2019-03-18 11:13:42 +0700

    homepage template

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -14,11 +14,30 @@
         height: 200px;
         background: #aaa;
     }
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        
+    }
+
+    li {
+    float: left;
+    }
+
+    li a {
+    display: block;
+   color:grey;
+    text-align: center;
+    padding: 16px;
+    text-decoration: none;
+    }
     </style>
 </head>
-<body>
+<body >
 
-    <div class="jumbotron text-center" style="margin-bottom:0">
+    <div class=" text-center" style="margin-bottom:0">
         <h1>Booklet</h1>
     </div>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
@@ -44,7 +63,7 @@
     <div class="container" style="margin-top:30px">
         <div class="row">
             <div class="col-xl col-lg-3 col-md-4 col-sm-6 pb-4">
-                <div class="card card-block">
+                <div class="card card-block shadow">
                     <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG" alt="Card image cap">
                     <div class="card-body" style="margin:30px">
                         <h5>Tittle</h5>
@@ -55,7 +74,7 @@
                 </div>
             </div>
             <div class="col-xl col-lg-3 col-md-4 col-sm-6 pb-4">
-                <div class="card card-block">
+                <div class="card card-block shadow">
                     <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG" alt="Card image cap">
                     <div class="card-body" style="margin:30px">
                         <h5>Tittle</h5>
@@ -66,7 +85,7 @@
                 </div>
             </div>
             <div class="col-xl col-lg-3 col-md-4 col-sm-6 pb-4">
-                <div class="card card-block">
+                <div class="card card-block shadow">
                     <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG" alt="Card image cap">
                     <div class="card-body" style="margin:30px">
                         <h5>Tittle</h5>
@@ -77,14 +96,146 @@
                 </div>
             </div> 
         </div>
-    </div>
-    <div class="row">
-        <div class="offset-md-9">
-            <a href="#" class="badge badge-light" style="font-size:24px;color:#808e95">More features </a>
+        <div class="row justify-content-end">
+            <div class="col-2">
+                <a href="#" class="badge badge-light" style="font-size:20px;color:#808e95">More features </a>
+            </div>
+        </div>
+
+        <div class="row justify-content-center" style="margin-top: 24px">
+            <div class="col-8 shadow mb-5">
+                <div>
+                    {{-- Member block --}}
+                    <div style="padding:8%" >
+                        <div class="row border border-secondary border-top-0 border-right-0 border-left-0">
+                            <div class="col-md-4 "style="font-size:1.5rem" >
+                                <ul>
+                                    <li>Member</li>
+                                <ul>
+                            </div>
+                            <div class="col-md-3 ml-auto m-2">
+                                <a href="#"  style="color:#808e95;">More features </a>
+                            </div>
+                            
+                            
+                            
+                        </div>
+                        <div class="media pt-4">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG" class="rounded align-self-start mr-3" alt="...">
+                            <div class="media-body">
+                                <p style="font-size:90%;color:#808e95">technology</p>
+                                <h5 class="mt-0">Top-aligned media</h5>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                </div>
+                        </div>
+                        <div class="media">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG" class="rounded align-self-start mr-3" alt="...">
+                            <div class="media-body">
+                                <p style="font-size:90%;color:#808e95">technology</p>
+                                <h5 class="mt-0">Top-aligned media</h5>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                </div>
+                        </div>
+                        <div class="media">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG" class="rounded align-self-start mr-3" alt="...">
+                            <div class="media-body">
+                                <p style="font-size:90%;color:#808e95">technology</p>
+                                <h5 class="mt-0">Top-aligned media</h5>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                </div>
+                        </div>
+                    </div>
+                    {{-- Member block --}}
+                </div>
+                <div>
+                    {{-- Fiction block --}}
+                    <div style="padding:8%" >
+                        <div class="row border border-secondary border-top-0 border-right-0 border-left-0">
+                            <div class="col-md-4" style="font-size:1.5rem">
+                                <ul>
+                                    <li>Member</li>
+                                <ul>
+                            </div>
+                            <div class="col-md-3 ml-auto m-2">
+                                <a href="#"  style="color:#808e95;">More features </a>
+                            </div>
+                            
+                            
+                            
+                        </div>
+                        <div class="media pt-4">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG" class="rounded align-self-start mr-3" alt="...">
+                            <div class="media-body">
+                                <p style="font-size:90%;color:#808e95">technology</p>
+                                <h5 class="mt-0">Top-aligned media</h5>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                </div>
+                        </div>
+                        <div class="media">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG" class="rounded align-self-start mr-3" alt="...">
+                            <div class="media-body">
+                                <p style="font-size:90%;color:#808e95">technology</p>
+                                <h5 class="mt-0">Top-aligned media</h5>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                </div>
+                        </div>
+                        <div class="media">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG" class="rounded align-self-start mr-3" alt="...">
+                            <div class="media-body">
+                                <p style="font-size:90%;color:#808e95">technology</p>
+                                <h5 class="mt-0">Top-aligned media</h5>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                </div>
+                        </div>
+                    </div>
+                    {{-- Fiction block --}}
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="shadow">
+                     <div class="p-2"style="font-size:1.5rem">
+                        <ul>
+                            <li>Most popular article</li>
+                        <ul>
+                    </div>
+                    <div class="row">
+                        <div class="col-10 ml-4" >
+                            <p style="color:#808e95">technology</p>
+                            <h5 >Top-aligned media</h5>
+                            <p>Cras sit amet nibh libero, in gravida nulla. </p>
+                                
+                        </div>
+                        <div class="col-10 ml-4" >
+                            <p style="color:#808e95">technology</p>
+                            <h5 >Top-aligned media</h5>
+                            <p>Cras sit amet nibh libero, in gravida nulla. </p>
+                                
+                        </div>
+                        <div class="col-10 ml-4" >
+                            <p style="color:#808e95">technology</p>
+                            <h5 >Top-aligned media</h5>
+                            <p>Cras sit amet nibh libero, in gravida nulla. </p>
+                                
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <ul>
+                        <li><a href="#">Help</a></li>
+                        <li><a href="#">Status</a></li>
+                        <li><a href="#">Writers</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Terms</a></li>
+                        <li><a href="#">About</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
-
-
+    
     {{-- i will do tmrrrrrrrrrrrrrrrrrrrrr --}}
     
 

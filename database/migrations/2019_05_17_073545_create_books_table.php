1e56e21863761a545563f339709240399a3018ac
phannsreyroth <phsreyroth@gmail.com>
2019-05-17 15:44:32 +0700

    database migration(Books,Iscategory,catogory)

---
+++ database/migrations/2019_05_17_073545_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Tittle');
+            $table->date('DateOfPublish');
+            $table->string('Content');
+            $table->integer('ViewCount');
+            $table->string('TotalRate');
+            $table->string('TotalView');
+            $table->integer('UserID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}

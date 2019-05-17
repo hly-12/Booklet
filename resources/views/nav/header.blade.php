25231b646710dd7dd6fd0d75d5111a8f2cd68e94
vindny <vindaramuny.vd@gmail.com>
2019-05-17 13:53:52 +0700

    database migration

--- resources/views/nav/header.blade.php
+++ resources/views/nav/header.blade.php
@@ -3,12 +3,12 @@
       <li ><img src="{{asset('img/logo.png')}}" width="65" height="70"></li>
       <li style="margin-top:15px;margin-left:10px"><h1>Booklet</h1></li>
       <li class="float-right" style="padding-right:10%;margin-top:3%;padding-left:10px;"><button type="button" class="btn btn-dark">Sign up</button></li>
-      <li class="float-right" style="margin-top:15px;padding-top:2%;"><h8 >Become a member<h8></li>
+      <li class="float-right" style="margin-top:15px;padding-top:1%;"><h8 style="font-weight: bold ;"><a href="/profile">Become a member<h8></a></li>
       <li class="float-right" style="margin-top:1%;"><a href="#" class="search_icon"><i class="fa fa-search" aria-hidden="true"></i></a></li>
     </ul>
 </div> 
 <nav class="navbar navbar-dark bg-dark">
-        <a class="nav-item nav-link text-ligh" style="padding-left:20%;color:white" href="#">Fictions</a>
+        <a class="nav-item nav-link text-ligh" style="padding-left:20%;color:white" href="/">Fictions</a>
         <a class="nav-item nav-link text-ligh" style="color:white"  href="/profile">Technology</a>
         <a class="nav-item nav-link text-ligh" style="color:white" href="/viewdata">Action</a>
         <a class="nav-item nav-link text-ligh" style="color:white" href="#">Sport</a>

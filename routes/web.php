cb3f60b7ee09bede9bcf727e9d974b005e1a1206
vindny <vindaramuny.vd@gmail.com>
2019-05-28 18:44:23 +0700

    fix conflict by magic

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get('/homepage', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/logout', function(){
+    Auth::logout();
+    return Redirect::to('/');
+ });
